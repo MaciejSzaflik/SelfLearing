6b8ae3033564e06584f257c0536119027bcaf760
MaciejSzaflik <maciej.szaflik@gmail.com>
2016-07-07 23:01:42 +0200

    project setup

---
+++ desktop/src/com/nobody/selflearing/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nobody.selflearing.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nobody.selflearing.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
